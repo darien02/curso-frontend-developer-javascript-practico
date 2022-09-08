a009614b6437ffae768a64eef4e5d8a86eb272b8
Darien <darienxx5@gmail.com>
2022-09-08 16:50:45 -0300

    se agrego detalles de productos

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const menuMobile = document.querySelector('.mobile-menu')
 //variables carrito
 const menuCarrito = document.querySelector('.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
+//selecionar div de producto 
+const cardsContainer = document.querySelector('.cards-container')
 
 //evento toggle menu email
 menuClickDesktop.addEventListener('click', toggleDesktopMenu)
@@ -16,7 +18,7 @@ botonMenuHam.addEventListener('click',toggleMobileMenu)
 menuCarrito.addEventListener('click', togglecarrito)
 
 function toggleDesktopMenu(){
-    isAsideMenuClosed = aside.classList.contains('inactive')
+        isAsideMenuClosed = aside.classList.contains('inactive')
     if(!isAsideMenuClosed){
         aside.classList.add('inactive')
     }
@@ -40,3 +42,72 @@ function togglecarrito() {
     aside.classList.toggle('inactive')
     
 }
+
+const productList =[]
+    productList.push({
+        name: 'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+
+    })
+    productList.push({
+        name: 'perro',
+        price: 150,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        
+    })
+    productList.push({
+        name: 'gato',
+        price: 130,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+        
+    })
+    
+    function renderProduct(arr) {
+        for (producto of productList){
+
+            const productCard = document.createElement('div')
+            productCard.classList.add('product-card')
+    
+            const productImg = document.createElement('img')
+            productImg.setAttribute('src', producto.image)
+    
+            const productInfo = document.createElement('div')
+            productInfo.classList.add('product-info')
+    
+            const productInfoDiv = document.createElement('div')
+    
+            const productPrice = document.createElement('p')
+            productPrice.innerText ='$'+producto.price
+            const productName = document.createElement('p')
+            productName.innerText = producto.name
+    
+            productInfoDiv.appendChild(productPrice)
+            productInfoDiv.appendChild(productName)
+    
+    
+            const productInfoFigure = document.createElement('figure');
+            const productImgCart = document.createElement('img');
+            productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    
+            productInfoFigure.appendChild(productImgCart)
+    
+            productInfo.appendChild(productInfoDiv)
+            productInfo.appendChild(productInfoFigure)
+    
+            productCard.appendChild(productImg)
+            productCard.appendChild(productInfo)
+    
+            cardsContainer.appendChild(productCard)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        } 
+    }
+    renderProduct(productList)
